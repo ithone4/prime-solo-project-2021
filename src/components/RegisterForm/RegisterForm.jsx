e67e651307641f88bbaff8ddb6f6f94cabd9bdf5
Sam Thone <ithone4@gmail.com>
2021-12-31 15:06:21 -0600

    cells successfully added to DB

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 function RegisterForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [cell, setCell] = useState('');
   const errors = useSelector((store) => store.errors);
   const dispatch = useDispatch();
 
@@ -15,6 +16,7 @@ function RegisterForm() {
       payload: {
         username: username,
         password: password,
+        cell: cell,
       },
     });
   }; // end registerUser
@@ -48,6 +50,18 @@ function RegisterForm() {
             value={password}
             required
             onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="cell">
+          Cell:
+          <input
+            type="cell"
+            name="cell"
+            value={cell}
+            required
+            onChange={(event) => setCell(event.target.value)}
           />
         </label>
       </div>
